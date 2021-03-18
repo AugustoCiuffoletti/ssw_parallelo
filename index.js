6e3aa88ab1cee5f52c7d6988f949b1d13a6444f6
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-18 12:31:08 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>Query in parallelo con le promise</h1>`;
 
-const API_KEY = 'd0fda39104b3c7c45fe031a5392964c1';
+const API_KEY = "d0fda39104b3c7c45fe031a5392964c1";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
 
 // Set the list of cities
-const citta = [ 'Milan', 'Turin', 'Pisa', 'Rome', 'Trento' ];
+const citta = ["Milan", "Turin", "Pisa", "Rome", "Trento"];
 // La funzione restituisce un array [ citta, temperatura ]
 function fetchTempForCity(city) {
-  return fetch(`${API_URL}&q=${encodeURIComponent(city)}`) // fetch restituisce una promise
-    .then(response => response.json())                     // anche la json()
-    .then(data => [ city, data.main.temp || null ]);       // passa un array di due posizioni
+  return fetch(`${API_URL}&q=${city}`) // fetch restituisce una promise
+    .then(response => response.json()) // anche la json()
+    .then(data => data.main.temp); // passa un array di due posizioni
 }
-Promise
-  .all(citta.map(fetchTempForCity))     // Applica all'array di Promise costruito con "map"
-  .then(temps => {          // temps e' l'array dei risultati di tutte le Promise    
-    return temps.reduce(		// utilizza il metodo reduce sull'array
-                            // il reducer aggiunge al dizionario "data" 
-      (data, [ city, temp ]) => { data[city] = temp; return data; },  
-      {});                              // "data" inizialmente vuoto
-    })
-  .then(console.log, console.error);
+
+async function fetchall() {
+  return Promise.all(citta.map(fetchTempForCity)); // Promette un array con tutte le temperature
+}
+
+fetchall().then(temps => {
+  // temps e' l'array dei risultati di tutte le Promise
+  console.log(temps);
+  var media = 0;
+  media = temps.reduce((media, data) => data + media) / temps.length;
+  console.log(media);
+});
