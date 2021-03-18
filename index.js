5fdbdca60dfa489609efb16466b5e4a7a9bb0f26
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-18 12:42:22 +0100

    2 files modified

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 import "./style.css";
 
 // Write TypeScript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>Query in parallelo con le promise</h1>`;
 
 const API_KEY = "d0fda39104b3c7c45fe031a5392964c1";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
@@ -13,18 +11,18 @@ const citta = ["Milan", "Turin", "Pisa", "Rome", "Trento"];
 // La funzione restituisce un array [ citta, temperatura ]
 function fetchTempForCity(city) {
   return fetch(`${API_URL}&q=${city}`) // fetch restituisce una promise
-    .then(response => response.json()) // anche la json()
-    .then(data => data.main.temp); // passa un array di due posizioni
+    .then( (response) => response.json()) // anche la json()
+    .then( (data) => data.main.temp); // passa un array di due posizioni
 }
-
-async function fetchall() {
-  return Promise.all(citta.map(fetchTempForCity)); // Promette un array con tutte le temperature
+// Questa funzione promette un array con le tutte temperature 
+function fetchall() {
+  return Promise.all(citta.map(fetchTempForCity));
 }
-
+// Definisce il metodo collegato al successo della fetchall
 fetchall().then(temps => {
   // temps e' l'array dei risultati di tutte le Promise
   console.log(temps);
   var media = 0;
   media = temps.reduce((media, data) => data + media) / temps.length;
-  console.log(media);
+  document.getElementById("app").innerHTML= "La temperatura media è di " + media + " gradi";
 });
