697a7987bb01bc2d51ec37a0b61a9eee3af5a80a
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-22 11:41:17 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import "./style.css";
 
 const API_KEY = "d0fda39104b3c7c45fe031a5392964c1";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
-
 // Set the list of cities
 const citta = ["Milan", "Turin", "Pisa", "Rome", "Trento"];
+media();
 // La funzione restituisce un array [ citta, temperatura ]
 async function fetchTempForCity(city) {
   let response = await fetch(API_URL + "&q=" + city); // fetch restituisce una promise
   if (response.ok) {
-    let json = await response.json();
+    let json = await response.json(); // json restituisce una promise
     return json.main.temp;
   } else {
     alert("Errore: " + response.status);
@@ -23,10 +23,10 @@ function fetchall() {
   return Promise.all(citta.map(fetchTempForCity));
 }
 // Definisce il metodo collegato al successo della fetchall
-fetchall().then(temps => {
-  // temps e' l'array dei risultati di tutte le Promise
-  var media = 0;
+async function media() {
+  let media = 0;
+  let temps = await fetchall(); // temps e' l'array dei risultati di tutte le Promise
   media = temps.reduce((media, data) => data + media) / temps.length;
   document.getElementById("app").innerHTML =
     "La temperatura media e' di " + media + " gradi";
-});
+}
