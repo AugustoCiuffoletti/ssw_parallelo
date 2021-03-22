df770f5546da6df0d7739bcd5a79d3564b24d4cf
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-22 14:37:58 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const API_KEY = "d0fda39104b3c7c45fe031a5392964c1";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
 // La lista di citta'
 const citta = ["Milan", "Turin", "Pisa", "Rome", "Trento"];
-media(); // Invocazione della funzione
+media(); // Calcolo della media
 // La funzione promette un array di temperature
 async function fetchTempForCity(city) {
   let response = await fetch(API_URL + "&q=" + city); // fetch() restituisce una promise
